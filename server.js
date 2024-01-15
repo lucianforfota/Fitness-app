150de7e10ae8c8d1d9a7c4405da902a26bef304e
Maria Condrea <maria.condrea@fortech.ro>
2024-01-15 10:45:06 +0200

    Login frontend and backend

--- server.js
+++ server.js
@@ -3,11 +3,46 @@ const bodyParser = require("body-parser");
 const cors = require("cors"); // Import the cors middleware
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Use bodyParser middleware to parse JSON requests
 app.use(bodyParser.json());
 app.use(cors());
+
+const users = [
+  {
+    id: 1,
+    username: "Mihai",
+    password: "Fragrance",
+    token: null,
+  },
+  {
+    id: 2,
+    username: "Maria",
+    password: "Lake",
+    token: null,
+  },
+];
+
+app.post("/login", (req, res) => {
+  // Check if the request body contains 'num1' and 'num2'
+  if (req.body && req.body.username && req.body.password) {
+    const foundUser = users.find(
+      (user) =>
+        user.username === req.body.username &&
+        user.password === req.body.password
+    );
+    if (foundUser) {
+      const token = (Math.random() + 1).toString(36).substring(7);
+      foundUser.token === token;
+      res.json({ token: token });
+    } else {
+      return res.status(401).json({ error: "Unathorized user" });
+    }
+  } else {
+    res.status(400).json({ error: "Missing parameters in request body" });
+  }
+});
 
 // POST endpoint to calculate the sum
 app.post("/sum", (req, res) => {
